e0331793b841ecc3adfda112096e840588335892
Royet Manalo <royetmanalo@gmail.com>
2022-09-13 20:28:45 +0800

    Prep for deployment

--- server/index.js
+++ server/index.js
@@ -19,6 +19,12 @@ app.use("/api/quests", require("./routes/questRoutes"));
 
 app.use(errorHandler);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "public")));
+  // app.use("/api/static", express.static(path.join(__dirname, "imageStatic")));
+  app.get(/.*/, (req, res) => res.sendFile(__dirname + "/public/index.html"));
+}
+
 app.listen(PORT, () => {
   console.log("Server running @ PORT:5000");
 });
